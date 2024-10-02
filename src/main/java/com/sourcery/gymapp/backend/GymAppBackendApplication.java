09f1554c5e3a1ac7caa033ee7930a491e7602104
Tymofii Skrypko <skrypko270901@gmail.com>
2024-10-02 12:14:07 +0200

    first BE setup

---
+++ src/main/java/com/sourcery/gymapp/backend/GymAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sourcery.gymapp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sourcery/gymapp/backend/GymAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sourcery.gymapp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
