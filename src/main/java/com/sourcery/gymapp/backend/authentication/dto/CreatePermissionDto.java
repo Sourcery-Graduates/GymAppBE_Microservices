bc42e9d0a95afe258acd1e41060558333743212d
Valentyn Bondarenko <murica.it@gmail.com>
2024-10-16 13:42:14 +0200

    Delete unused API endpoints for permissions

--- src/main/java/com/sourcery/gymapp/backend/authentication/dto/CreatePermissionDto.java
+++
@@ -1,9 +0,0 @@
-package com.sourcery.gymapp.backend.authentication.dto;
-
-import java.io.Serializable;
-
-
-public record CreatePermissionDto(
-        String name,
-        String description
-) implements Serializable {}
--- src/main/java/com/sourcery/gymapp/backend/authentication/repo/PermissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sourcery.gymapp.backend.authentication.repo;
-
-import com.sourcery.gymapp.backend.authentication.model.Permission;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface PermissionRepository extends JpaRepository<Permission, UUID> {
-}
