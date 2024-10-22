31cca75b7875bb5ca1bfbf229d58c092ce202b76
Tymofii Skrypko <skrypko270901@gmail.com>
2024-10-22 13:56:53 +0200

    GYM-50 added JwtIllegalStateException to UserProfile module

--- src/main/java/com/sourcery/gymapp/backend/userProfile/service/ProfileCurrentUserService.java
+++ src/main/java/com/sourcery/gymapp/backend/userProfile/service/ProfileCurrentUserService.java
@@ -1,7 +1,7 @@
 package com.sourcery.gymapp.backend.userProfile.service;
 
 import java.util.UUID;
-
+import com.sourcery.gymapp.backend.userProfile.exception.JwtIllegalStateException;
 import com.sourcery.gymapp.backend.userProfile.exception.UserNotAuthenticatedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,13 +11,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProfileCurrentUserService {
 
-    //TODO: add to module-local exception handler when handler is created
     public UUID getCurrentUserId() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null && authentication.getPrincipal() instanceof Jwt jwt) {
             UUID userId = jwt.getClaim("userId");
             if (userId == null) {
-                throw new IllegalStateException("There is no userId in JWT");
+                throw new JwtIllegalStateException("There is no userId in JWT");
             }
             return userId;
         }
