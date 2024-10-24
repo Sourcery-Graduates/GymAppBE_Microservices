78a316609ab915fbcd7e29f2863abe7a287b801b
przemyslawren <przemyslaw.ren@gmail.com>
2024-10-24 16:04:35 +0200

    update imports

--- src/main/java/com/sourcery/gymapp/backend/workout/controller/RoutineExerciseController.java
+++ src/main/java/com/sourcery/gymapp/backend/workout/controller/RoutineExerciseController.java
@@ -2,11 +2,15 @@ package com.sourcery.gymapp.backend.workout.controller;
 
 import com.sourcery.gymapp.backend.workout.dto.CreateRoutineExerciseDto;
 import com.sourcery.gymapp.backend.workout.dto.CreateRoutineGridExerciseDto;
-import com.sourcery.gymapp.backend.workout.dto.ResponseRoutineExerciseDto;
 import com.sourcery.gymapp.backend.workout.dto.ResponseRoutineGridExerciseDto;
 import com.sourcery.gymapp.backend.workout.service.RoutineExerciseService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 import java.util.UUID;
