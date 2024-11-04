50720e6849458065896791a7abff206a54d001bc
Tymofii Skrypko <skrypko270901@gmail.com>
2024-11-04 14:05:08 +0100

    GYM-97 minor fix

--- src/main/java/com/sourcery/gymapp/backend/workout/dto/RoutinePageDto.java
+++ src/main/java/com/sourcery/gymapp/backend/workout/dto/RoutinePageDto.java
@@ -1,10 +1,10 @@
-    package com.sourcery.gymapp.backend.workout.dto;
+package com.sourcery.gymapp.backend.workout.dto;
 
-    import java.util.List;
+import java.util.List;
 
-    public record RoutinePageDto(
-            int totalPages,
-            long totalElements,
-            List<ResponseRoutineDto> data
-    ) {
-    }
+public record RoutinePageDto(
+        int totalPages,
+        long totalElements,
+        List<ResponseRoutineDto> data
+) {
+}
