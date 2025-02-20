1bd723422dc093a10d90727055933f9a822c2960
Valentyn Bondarenko <murica.it@gmail.com>
2025-02-20 13:20:32 +0100

    Enable kafka streams in the app

---
+++ src/main/java/com/sourcery/gymapp/backend/globalconfig/KafkaStreamsConfig.java
@@ -0,0 +1,9 @@
+package com.sourcery.gymapp.backend.globalconfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+
+@Configuration
+@EnableKafkaStreams
+public class KafkaStreamsConfig {
+
+}
--- src/main/java/com/sourcery/gymapp/backend/kafka/streams/NotificationsLikesAggregator.java
+++ src/main/java/com/sourcery/gymapp/backend/kafka/streams/NotificationsLikesAggregator.java
@@ -5,12 +5,18 @@ import com.sourcery.gymapp.backend.events.RoutineLikeEvent;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.TimeWindows;
+import org.apache.kafka.streams.kstream.Windowed;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.util.UUID;
 
 @Component
@@ -19,15 +25,51 @@ public class NotificationsLikesAggregator {
     private String INPUT_TOPIC;
     @Value("${spring.kafka.topics.likes-notifications}")
     private String OUTPUT_TOPIC;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Bean
-    public KStream<UUID, String> notificationLikesStream(StreamsBuilder builder) {
-        KStream<UUID, String> stream = builder.stream(INPUT_TOPIC, Consumed.with(
-                new Serdes.UUIDSerde(), new Serdes.StringSerde()
-        ));
-        ObjectMapper objectMapper = new ObjectMapper();
+    public KTable<UUID, Long> notificationLikesStream(StreamsBuilder builder) {
 
-        KTable<UUID, String> reactionCounts = stream.groupBy()
+        KStream<UUID, String> textLines = builder.stream(
+                INPUT_TOPIC
+        );
 
+        KTable<UUID, Long> outputStream = textLines
+                .groupByKey()
+                .count();
+
+        outputStream.toStream().to(OUTPUT_TOPIC, Produced.with(Serdes.UUID(), Serdes.Long()));
+        return outputStream;
+
+//        KStream<UUID, RoutineLikeEvent> inputStream = builder
+//                .stream(INPUT_TOPIC,
+//                        Consumed.with(new Serdes.UUIDSerde(), new Serdes.StringSerde())
+//                ).mapValues((value) -> {
+//                    try {
+//                        return objectMapper.readValue(value, RoutineLikeEvent.class);
+//                    } catch (Exception e) {
+//                        return null;
+//                    }
+//                }).filter((key, value) -> value != null);
+//
+//
+//        KTable<Windowed<UUID>, Long> reactionCounts = inputStream
+//                .groupByKey(Grouped.with(new Serdes.UUIDSerde(), Serdes.serdeFrom(RoutineLikeEvent.class)))
+//                .windowedBy(TimeWindows.of(Duration.ofDays(7)))
+//                .count(Materialized.as("likes-aggregated-store"));
+//
+//        KStream<UUID, String> outputStream = reactionCounts
+//                .toStream((windowedKey, value) -> windowedKey.key())
+//                .mapValues(value -> {
+//                    try {
+//                        return objectMapper.writeValueAsString(new LikeNotification(value));
+//                    } catch (Exception e) {
+//                        return "{}";
+//                    }
+//                });
+//
+//        outputStream.to(OUTPUT_TOPIC, Produced.with(new Serdes.UUIDSerde(), Serdes.String()));
+
+//        return outputStream;
     }
 }
