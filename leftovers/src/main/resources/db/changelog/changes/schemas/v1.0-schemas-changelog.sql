4bcc84e915764b4479ea9c803054fdd39193f722
Valentyn Bondarenko <murica.it@gmail.com>
2025-05-14 18:39:46 +0200

    Deleted migration leftovers

--- leftovers/src/main/resources/db/changelog/changes/schemas/v1.0-schemas-changelog.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
-
---changeset przemyslawren:create-schemas
-CREATE SCHEMA IF NOT EXISTS public;
-CREATE SCHEMA IF NOT EXISTS user_auth;
-CREATE SCHEMA IF NOT EXISTS user_profiles;
-CREATE SCHEMA IF NOT EXISTS workout_data;
-CREATE SCHEMA IF NOT EXISTS shared_links;
