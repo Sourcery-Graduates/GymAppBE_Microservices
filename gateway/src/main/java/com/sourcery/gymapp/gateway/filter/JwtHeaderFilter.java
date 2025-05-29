c02bd13a7fbf2956636d93ca48c564e39955ebaf
przemyslawren <przemyslaw.ren@gmail.com>
2025-05-29 14:32:33 +0200

    update jwt header filter

--- gateway/src/main/java/com/sourcery/gymapp/gateway/filter/JwtHeaderFilter.java
+++ gateway/src/main/java/com/sourcery/gymapp/gateway/filter/JwtHeaderFilter.java
@@ -1,33 +1,35 @@
 package com.sourcery.gymapp.gateway.filter;
 
 import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.security.core.Authentication;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
 
-//@Component
-//public class JwtHeaderFilter implements GatewayFilter {
-//
-//    @Override
-//    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-//        Authentication authentication = ReactiveSecurityContextHolder.getContext()
-//                .map(SecurityContext::getAuthentication)
-//                .block(); // NIE w produkcji — użyj Reactive stylu, to dla uproszczenia
-//
-//        if (authentication instanceof JwtAuthenticationToken jwtAuth) {
-//            Jwt jwt = jwtAuth.getToken();
-//            String userId = jwt.getClaimAsString("sub");
-//            exchange.getRequest()
-//                    .mutate()
-//                    .header("X-User-Id", userId)
-//                    .build();
-//        }
-//
-//        return chain.filter(exchange);
-//    }
-//}
+@Component
+public class JwtHeaderFilter extends AbstractGatewayFilterFactory<Object> {
+
+    @Override
+    public GatewayFilter apply(Object config) {
+        return (exchange, chain) -> {
+
+            return ReactiveSecurityContextHolder.getContext()
+                    .map(SecurityContext::getAuthentication)
+                    .filter(auth -> auth instanceof JwtAuthenticationToken)
+                    .cast(JwtAuthenticationToken.class)
+                    .map(jwtAuth -> {
+                        Jwt jwt = jwtAuth.getToken();
+                        String userId = jwt.getClaimAsString("userId");
+                        return exchange.mutate()
+                                .request(exchange.getRequest().mutate()
+                                        .header("X-User-Id", userId)
+                                        .build())
+                                .build();
+                    })
+                    .defaultIfEmpty(exchange)
+                    .flatMap(chain::filter);
+        };
+    }
+}
