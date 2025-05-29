13501c95eecc1cad60e784abe91cad79e29f5b81
przemyslawren <przemyslaw.ren@gmail.com>
2025-05-29 14:38:31 +0200

    update JwtHeaderFilter

--- gateway/src/main/java/com/sourcery/gymapp/gateway/filter/JwtHeaderFilter.java
+++ gateway/src/main/java/com/sourcery/gymapp/gateway/filter/JwtHeaderFilter.java
@@ -13,23 +13,20 @@ public class JwtHeaderFilter extends AbstractGatewayFilterFactory<Object> {
 
     @Override
     public GatewayFilter apply(Object config) {
-        return (exchange, chain) -> {
-
-            return ReactiveSecurityContextHolder.getContext()
-                    .map(SecurityContext::getAuthentication)
-                    .filter(auth -> auth instanceof JwtAuthenticationToken)
-                    .cast(JwtAuthenticationToken.class)
-                    .map(jwtAuth -> {
-                        Jwt jwt = jwtAuth.getToken();
-                        String userId = jwt.getClaimAsString("userId");
-                        return exchange.mutate()
-                                .request(exchange.getRequest().mutate()
-                                        .header("X-User-Id", userId)
-                                        .build())
-                                .build();
-                    })
-                    .defaultIfEmpty(exchange)
-                    .flatMap(chain::filter);
-        };
+        return (exchange, chain) -> ReactiveSecurityContextHolder.getContext()
+                .map(SecurityContext::getAuthentication)
+                .filter(auth -> auth instanceof JwtAuthenticationToken)
+                .cast(JwtAuthenticationToken.class)
+                .map(jwtAuth -> {
+                    Jwt jwt = jwtAuth.getToken();
+                    String userId = jwt.getClaimAsString("userId");
+                    return exchange.mutate()
+                            .request(exchange.getRequest().mutate()
+                                    .header("X-User-Id", userId)
+                                    .build())
+                            .build();
+                })
+                .defaultIfEmpty(exchange)
+                .flatMap(chain::filter);
     }
 }
