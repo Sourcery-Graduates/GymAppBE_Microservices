b0d2e76ac5d6f80f0dbe5862153e596d4b1246f1
przemyslawren <przemyslaw.ren@gmail.com>
2025-05-29 14:32:20 +0200

    update current user services

--- userProfile/src/main/java/com/sourcery/gymapp/userProfile/service/CurrentUserService.java
+++ userProfile/src/main/java/com/sourcery/gymapp/userProfile/service/CurrentUserService.java
@@ -2,18 +2,23 @@ package com.sourcery.gymapp.userProfile.service;
 
 import java.util.UUID;
 
-import com.sourcery.gymapp.userProfile.config.AuditorConfig;
-import com.sourcery.gymapp.userProfile.config.KafkaProcessingContext;
+import com.sourcery.gymapp.userProfile.exception.UserProfileRuntimeException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Service
 public class CurrentUserService {
 
     public UUID getCurrentUserId() {
-        if (KafkaProcessingContext.isKafkaProcessing()) {
-            return AuditorConfig.AuditorAwareImpl.getSystemUserUUID();
-        }
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
 
-        return UUID.randomUUID(); //TODO: Current user id placeholder
+        String userIdHeader = request.getHeader("X-User-Id");
+
+        if (userIdHeader == null) {
+            throw new UserProfileRuntimeException("Missing X-User-Id Header");
+        }
+        return UUID.fromString(userIdHeader);
     }
 }
--- workout/src/main/java/com/sourcery/gymapp/workout/service/CurrentUserService.java
+++ workout/src/main/java/com/sourcery/gymapp/workout/service/CurrentUserService.java
@@ -4,13 +4,11 @@ import java.util.UUID;
 
 import com.sourcery.gymapp.workout.exception.WorkoutRuntimeException;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Service
-@Slf4j
 public class CurrentUserService {
 
     public UUID getCurrentUserId() {
@@ -21,7 +19,6 @@ public class CurrentUserService {
         if (userIdHeader == null) {
             throw new WorkoutRuntimeException("Missing X-User-Id Header");
         }
-        log.info("Id Header: {}", userIdHeader);
         return UUID.fromString(userIdHeader);
     }
 }
